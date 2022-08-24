18a9d0c0134f98fe207343c6428826844961d7d0
Francisco Javier Martinez <francisco.mv@globant.com>
2022-08-24 18:46:16 -0500

    Set Navigation in App

--- Shared/ScrumAppApp.swift
+++ Shared/ScrumAppApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct ScrumAppApp: App {
     var body: some Scene {
         WindowGroup {
-            MeetingView()
+            NavigationView{
+                ScrumsView(scrums: DailyScrum.sampleData)
+            }
+            
         }
     }
 }
