605d55a363cf6b6a08ddac9dce9d65f42a4fcb92
Francisco Javier Martinez <francisco.mv@globant.com>
2022-08-23 20:55:17 -0500

    Initial Commit

---
+++ Shared/ScrumAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumAppApp.swift
+//  Shared
+//
+//  Created by Francisco Javier Martinez Vergara on 23/08/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
